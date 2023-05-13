958baca3c1601dd0199c0dcee229697d4b993b00
Jonas Andersson <jonas.andersson@toxic.se>
2023-05-13 22:29:23 +0200

    Add style and additional functions to pagination

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -15,9 +15,39 @@ const Pagination = ({ pokemonsPerPage, pokemons, currentPage, paginate}) => {
     paginate(pageNumber);
   }
 
+  const RenderPrevious = () => {
+    if(currentPage !== 1){
+      return(
+        <li className="page-list-item">
+                <div onClick={() => handlePreviousClick(currentPage)} className="page-number">
+                  <FaChevronLeft style={{ color: 'blanchedalmond', fontSize: '1.05rem', backGroundColor:'#e49f6a', padding: '6px 2px 0 0' }} />
+                </div>
+        </li>
+      ) 
+    }
+    else{ return(null)}
+  }
   const handlePreviousClick = (page) => {
     if (page > 1) {
       paginate(page - 1);
+    }
+  };
+
+  const RenderNext = () => {
+    if(currentPage !== numberOfPages){
+      return(
+        <li className="page-list-item">
+        <div onClick={() => handleNextClick(currentPage)} className="page-number previous-next-btn">
+          <FaChevronRight style={{ color: 'blanchedalmond', fontSize: '1.05rem', backGroundColor:'#e49f6a', padding: '6px 0 0 2px' }} />
+        </div>
+      </li>
+      ) 
+    }
+    else{ return(null)}
+  }
+  const handleNextClick = (page) => {
+    if (page < numberOfPages) {
+      paginate(page + 1);
     }
   };
 
@@ -25,22 +55,15 @@ const Pagination = ({ pokemonsPerPage, pokemons, currentPage, paginate}) => {
     <>
       <div className="pagination">
         <ul className="page-list">
-        <li className="previous-page-list-item">
-            <button
-              onClick={() => handlePreviousClick(currentPage)}
-              disabled={currentPage === 1}
-              className="page-number"
-            >
-              <FaChevronLeft className='page-list-item' />
-            </button>
-          </li>
+          <RenderPrevious />
           {pageNumbers.map( number => (
-            <li key={number} className="page-list-item">
-              <a href='!#' onClick={ () => handlePageClick(number)} className="page-number">
+            <li key={number} className={number === currentPage ? "page-list-item active" : "page-list-item"}>
+              <div onClick={ () => handlePageClick(number)} className="page-number">
                 {number}
-              </a>
+              </div>
             </li>
             ))}
+         <RenderNext />
         </ul>
       </div>
     </>

ab793cac967a328c4984beec9458e47e6055ce91
Jonas Andersson <jonas.andersson@toxic.se>
2023-05-13 09:36:47 +0200

    Fix issues with adding and removing cards to favorites

--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -1,57 +1,58 @@
 import './PokemonCard.css';
-import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { storeList, storeQuantity } from '../../Features/FavoritesData/FavoritesData.slice';
-import * as Constants from '../../Constants'
+import { storeList, storeQuantity, storeSelected } from '../../Features/FavoritesData/FavoritesData.slice';
+import * as Constants from '../../Constants';
 import Btn from '../Button/Btn';
 
 const PokemonCard = ({ id, image, name, type, object }) => {
+  const style = type + " pokemon-card";
+  const pokemon = object;
+  const pokemonId = object.id;
+  const favorites = useSelector((state) => state.favoritesData);
+  const favoritesList = [...favorites.list];
+  const selectedList = [...favorites.selected];
+  const isSelected = selectedList.includes(pokemonId); 
+  const dispatch = useDispatch();
 
-const style = type + " pokemon-card";
-const pokemon = object;
-const [favorite, setFavorite] = useState("");
-const favorites = useSelector((state) => state.favoritesData);
-const favoritesList = [...favorites.list];
-const dispatch = useDispatch()
-
-const handleClick = () => {
-    if ( favoritesList.length === 0){
-       favoritesList.push(pokemon);
-       console.log("array", favoritesList.length)
-       dispatch(storeList(favoritesList))
-       dispatch(storeQuantity(favoritesList.length))
-       setFavorite("selected");
-    }
-    else{
-      if (
-        favoritesList.includes(pokemon)){
+  const handleClick = () => {
+    if (favoritesList.length === 0) {
+      favoritesList.push(pokemon);
+      selectedList.push(pokemonId);
+      dispatch(storeList(favoritesList));
+      dispatch(storeQuantity(favoritesList.length));
+      dispatch(storeSelected(selectedList));
+    } else {
+      if (favoritesList.includes(pokemon)) {
         const index = favoritesList.indexOf(pokemon);
-        const updatedFavorites = favoritesList.splice(index, 1)
-        dispatch(storeList(updatedFavorites))
-        dispatch(storeQuantity(updatedFavorites.length))
-        setFavorite("");
-      } 
-      else{
-        favoritesList.push(object);
+        favoritesList.splice(index, 1);
+        selectedList.splice(index, 1); 
         dispatch(storeList(favoritesList));
-        dispatch(storeQuantity(favoritesList.length))
-        setFavorite("selected");
+        dispatch(storeQuantity(favoritesList.length));
+        dispatch(storeSelected(selectedList));
+      } else {
+        favoritesList.push(object);
+        selectedList.push(pokemonId);
+        dispatch(storeList(favoritesList));
+        dispatch(storeQuantity(favoritesList.length));
+        dispatch(storeSelected(selectedList));
       }
     }
+  };
 
-}
   return (
     <div className={style}>
       <div className="pokemon-card-image-container">
         <img src={image} alt={name} />
       </div>
       <div className="pokemon-card-text-container">
-        <div className="number"><h5>#0{id}</h5></div>
+        <div className="number">
+          <h5>#{id}</h5>
+        </div>
         <Btn
-            className="pokemon-card-add-to-favorites"
-            text={Constants.components.addToFavorites}
-            whenClicked={handleClick}
-            state={favorite}
+          className="pokemon-card-add-to-favorites"
+          text={Constants.components.addToFavorites}
+          whenClicked={handleClick}
+          state={isSelected ? 'selected' : ''}
         />
         <div className="detail-wrapper">
           <h3>{name}</h3>
@@ -59,8 +60,7 @@ const handleClick = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PokemonCard
-
+export default PokemonCard;
